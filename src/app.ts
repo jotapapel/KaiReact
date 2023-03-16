0eb7e88757fce4a366d675dcd3af78d33080748f
José M. Meza <j.mezaortiz@icloud.com>
2023-03-16 16:27:07 -0300

    tuesday changes

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import ReactElement = React.Element;
 
-Foundation.SoftkeyHandler.bind(Foundation.KeyboardKey.Enter, 'Log',  function () {
+SoftkeyHandler.bind(KeyboardKey.Enter, 'Log',  function () {
 	this.element.labelCenter = 'Logged';
 });
--- src/js/foundation.ts
+++ src/js/foundation.ts
@@ -1,66 +1,64 @@
-namespace Foundation {
-	export enum KeyboardKey {
-		SoftLeft = 'SoftLeft',
-		Enter = 'Enter',
-		SoftRight = 'SoftRight',
-		ArrowRight = 'ArrowRight',
-		ArrowUp = 'ArrowUp',
-		ArrowLeft = 'ArrowLeft',
-		ArrowDown = 'ArrowDown',
-		Call = 'Call',
-		Backspace = 'Backspace'
-	}
+enum KeyboardKey {
+	SoftLeft = 'SoftLeft',
+	Enter = 'Enter',
+	SoftRight = 'SoftRight',
+	ArrowRight = 'ArrowRight',
+	ArrowUp = 'ArrowUp',
+	ArrowLeft = 'ArrowLeft',
+	ArrowDown = 'ArrowDown',
+	Call = 'Call',
+	Backspace = 'Backspace'
+}
 
-	export namespace KeyboardListener {
-		let methods: {[index: string]: Function} = {};
-		document.addEventListener('keypress', (event) => {
-			if (KeyboardKey[event.key]) methods[event.key]();
-		});
-		export function add (key: KeyboardKey, method: Function) {
-			methods[key] = method;
+namespace KeyboardListener {
+	let methods: {[index: string]: Function} = {};
+	document.addEventListener('keypress', (event) => {
+		if (KeyboardKey[event.key]) methods[event.key]();
+	});
+	export function add (key: KeyboardKey, method: Function) {
+		methods[key] = method;
+	}
+}
+
+class SoftkeyHandler {
+	static element: React.Element = new React.Element({
+		selector: '#kai-softkey-container',
+		data: {
+			labelLeft: '',
+			labelCenter: '',
+			labelRight: ''
+		},
+		template: `
+			<label id="kai-softkey-left">{{labelLeft}}</label>
+			<label id="kai-softkey-center">{{labelCenter}}</label>
+			<label id="kai-softkey-right">{{labelRight}}</label>
+		`
+	});
+	static set visible (value: boolean) {
+		switch (value) {
+			case false:
+				SoftkeyHandler.element.innerElement.classList.add('hidden');
+				break;
+			case true:
+				SoftkeyHandler.element.innerElement.classList.remove('hidden');
+				break;
 		}
 	}
-
-	export class SoftkeyHandler {
-		static element: React.Element = new React.Element({
-			selector: '#kai-softkey-container',
-			data: {
-				labelLeft: '',
-				labelCenter: '',
-				labelRight: ''
-			},
-			template: `
-				<label id="kai-softkey-left">{{labelLeft}}</label>
-				<label id="kai-softkey-center">{{labelCenter}}</label>
-				<label id="kai-softkey-right">{{labelRight}}</label>
-			`
-		});
-		static set visible (value: boolean) {
-			switch (value) {
-				case false:
-					SoftkeyHandler.element.innerElement.classList.add('hidden');
-					break;
-				case true:
-					SoftkeyHandler.element.innerElement.classList.remove('hidden');
-					break;
-			}
-		}
-		static get visible () {
-			return SoftkeyHandler.element.HTMLElement.classList.contains('hidden');
-		}
-		static bind (key: KeyboardKey, label: string, method: Function) {
-			switch (key) {
-				case KeyboardKey.SoftLeft:
-					SoftkeyHandler.element.labelLeft = label;
-					break;
-				case KeyboardKey.Enter:
-					SoftkeyHandler.element.labelCenter = label;
-					break;
-				case KeyboardKey.SoftRight:
-					SoftkeyHandler.element.labelRight = label;
-					break;
-			};
-			KeyboardListener.add(key, method.bind(this));
-		}
+	static get visible () {
+		return SoftkeyHandler.element.HTMLElement.classList.contains('hidden');
+	}
+	static bind (key: KeyboardKey, label: string, method: Function) {
+		switch (key) {
+			case KeyboardKey.SoftLeft:
+				SoftkeyHandler.element.labelLeft = label;
+				break;
+			case KeyboardKey.Enter:
+				SoftkeyHandler.element.labelCenter = label;
+				break;
+			case KeyboardKey.SoftRight:
+				SoftkeyHandler.element.labelRight = label;
+				break;
+		};
+		KeyboardListener.add(key, method.bind(this));
 	}
 }
